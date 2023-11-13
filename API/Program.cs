0b72485573fc03c0eb44f65992675d7e510d3910
Tina <hnewbu1@gmail.com>
2023-11-12 20:38:01 -0500

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +20,8 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
